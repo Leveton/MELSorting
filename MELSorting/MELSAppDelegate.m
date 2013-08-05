cf1e6f7843fdab4b30c943761d969605699728ca
Leveton <mleveton@gmail.com>
2013-08-04 22:45:06 -0400

    added screenshot

--- MELSorting/MELSAppDelegate.m
+++ MELSorting/MELSAppDelegate.m
@@ -16,7 +16,7 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.viewController = [[MELSViewController alloc] initWithNibName:@"MELSViewController" bundle:nil];
+    self.viewController = [[MELSViewController alloc] init];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
